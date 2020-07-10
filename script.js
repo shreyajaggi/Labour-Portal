f6bf94c2e878534c3b139598cd9bf151f40eb5a4
Sophia Garg <56688049+SOPHIA2401@users.noreply.github.com>
2020-07-10 18:16:31 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,49 @@
+window.onload=()=>{
+    generateCaptcha();
+}
+function generateCaptcha(){
+    var code='';
+    var letters="abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+    for (let i = 0; i < 5; i++) {
+        var captchaNumber = letters[Math.ceil(Math.random()*62)];
+        if(captchaNumber)
+            code+=captchaNumber;
+       }
+    document.getElementById("txtCaptcha").value = code;
+    document.getElementById("CaptchaDiv").innerHTML = code;
+}
+function checkform(){
+    var theform = document.forms['loginForm'];
+    var why = "";
+    var m=theform.contact.value.match(/^(7|8|9)\d{9}/);
+    if(m==null){
+        var c=document.getElementById("contact");
+        c.focus();
+        return false;
+    }
+    if(theform.verificationCode.value != ""){
+        if(ValidCaptcha(theform.verificationCode.value) == false){
+            alert("- The CAPTCHA Code Does Not Match.\n");
+            document.getElementById("verificationCode").focus();
+            generateCaptcha();
+            return false;
+        }
+    }
+    return true;
+}
+    // Validate input against the generated number
+    function ValidCaptcha(){
+        var str1 = removeSpaces(document.getElementById('txtCaptcha').value);
+        var str2 = removeSpaces(document.getElementById('verificationCode').value);
+        if (str1 == str2){
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+    // Remove the spaces from the entered and generated code
+    function removeSpaces(string){
+        return string.split(' ').join('');
+    }
+// }
